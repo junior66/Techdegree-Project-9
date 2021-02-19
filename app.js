159e83c87c761a64bb7d8b32eb0ebf72ed0ce9a4
junior66 <jraidee@gmail.com>
2021-02-19 00:09:07 -0500

    updated html, css, js

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
+let btnLive1 = document.querySelector('.btn-live1');
+let btnCode1 = document.querySelector('.btn-code1');
+
+let btnLive2 = document.querySelector('.btn-live2');
+let btnCode2 = document.querySelector('.btn-code2');
+
+let btnLive3 = document.querySelector('.btn-live3');
+let btnCode3 = document.querySelector('.btn-code3');
+
+let btnLive4 = document.querySelector('.btn-live4');
+let btnCode4 = document.querySelector('.btn-code4');
+
+let btnLive5 = document.querySelector('.btn-live5');
+let btnCode5 = document.querySelector('.btn-code5');
+
+btnLive1.addEventListener('click', () => {
+    window.open("https://junior66.github.io/Techdegree-Project-3/", '_blank');
+});
+
+btnCode1.addEventListener('click', () => {
+    window.open("https://github.com/junior66/Techdegree-Project-3", '_blank');
+});
+
+btnLive2.addEventListener('click', () => {
+    window.open("https://junior66.github.io/Techdegree-Project-4/style_guide/", '_blank');
+});
+
+btnCode2.addEventListener('click', () => {
+    window.open("https://github.com/junior66/Techdegree-Project-4", '_blank');
+});
+
+btnLive3.addEventListener('click', () => {
+    window.open("https://junior66.github.io/Techdegree-Project-6/", '_blank');
+});
+
+btnCode3.addEventListener('click', () => {
+    window.open("https://github.com/junior66/Techdegree-Project-6", '_blank');
+});
+
+btnLive4.addEventListener('click', () => {
+    window.open("https://junior66.github.io/Techdegree-Project-8/", '_blank');
+});
+
+btnCode4.addEventListener('click', () => {
+    window.open("https://github.com/junior66/Techdegree-Project-8", '_blank');
+});
+
+btnLive5.addEventListener('click', () => {
+    window.open("https://junior66.github.io/Techdegree-Project-7-1/", '_blank');
+});
+
+btnCode5.addEventListener('click', () => {
+    window.open("https://github.com/junior66/Techdegree-Project-7-1", '_blank');
+});
+
+
+
 const user = document.getElementById( "userField" );
 const message = document.getElementById( "messageField" );
 const send = document.getElementById( "send" );
@@ -13,3 +70,21 @@ send.addEventListener( 'click' , () => {
     } else {
     alert( `Message successfully sent to: ${user.value}` )};
     })
+
+
+    // 
+    // 
+
+    // 
+    // 
+
+    // 
+    // 
+
+    // 
+    // 
+
+    // 
+    // 
+
+
